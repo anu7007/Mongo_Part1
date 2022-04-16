173b2ca80506544fc1d602056c79573db10f3c01
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-04-16 11:43:08 +0530

    mongo db Part 1

---
+++ src/app/controllers/IndexController.php
@@ -0,0 +1,81 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+use GuzzleHttp\Client;
+
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+    }
+    public function addProductAction()
+    {
+        if ($this->request->getPost()) {
+            $keyForFieldName = $this->request->getPost('field_name');
+            $valueForFieldValue = $this->request->getPost('field_value');
+            $variationSize = $this->request->getPost('size');
+            $variationColor = $this->request->getPost('color');
+            $variationPrice = $this->request->getPost('variation_price');
+            $additional_fields = array_combine($keyForFieldName, $valueForFieldValue);
+            $variations = array(
+                "size" => $variationSize,
+                "color" => $variationColor,
+                "price" => $variationPrice
+            );
+
+            $postdata = $this->request->getPost();
+            if (!(empty($postdata['product_name']) || empty($postdata['tags']) || empty($postdata['price']) || empty($postdata['stock']))) {
+
+                $success = $document = array(
+                    "product_name" => $postdata['product_name'],
+                    "product_category" => $postdata['tags'],
+                    "price" => $postdata['price'],
+                    "stock" => $postdata['stock'],
+                    "additional_fields" => $additional_fields,
+                    "variations" => $variations
+                );
+                $this->mongo->insertOne($document);
+            }
+
+            if ($success) {
+                $this->session->msg = "<p class='alert-success'>Product added successfully!!</p>";
+                $this->response->redirect('/index');
+            } else {
+                $this->session->msg = "<p class='alert-danger'>*All fields are required!!</p>";
+                $this->response->redirect('/index');
+            }
+
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        }
+    }
+    public function productsListAction()
+    {
+        $resultt = $this->mongo->find();
+        $this->view->allResult = $resultt;
+        // json_decode(json_encode($resultt), true);
+        if ($this->request->getPost('search')) {
+            $searchByProductName = $this->request->getPost('searchByProductName');
+            $result = array();
+            foreach ($resultt as $k => $v) {
+
+                if (strtolower($v->product_name) == strtolower($searchByProductName)) {
+                    array_push($result, $v);
+                } else {
+                    $this->view->error = "<h3 class='alert alert-danger'>No Product Found !!</h3>";
+                }
+            }
+            $this->view->result = $result;
+        }
+        if ($this->request->getPost('delete')) {
+            $id = $this->request->getPost('id');
+            $data = $this->mongo->deleteOne([
+                "_id" => new MongoDB\BSON\ObjectID($id)
+            ]);
+
+            $this->response->redirect('/index/productsList');
+        }
+    }
+}
